e656045903a7dbdd6be145c7c373834521d09a91
Balazs Szamody <bszamody@gmail.com>
2020-10-13 11:08:50 +1100

    Xcode 11 support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,11 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Configurable",
-            dependencies: [],
-            resources:[.process("Resources")]),
+            dependencies: []),
         .testTarget(
             name: "ConfigurableTests",
-            dependencies: ["Configurable"],
-            resources:[.process("Resources")]),
+            dependencies: ["Configurable"]),
     ]
 )
